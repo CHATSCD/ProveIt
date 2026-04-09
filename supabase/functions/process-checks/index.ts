1367bf0b8c5c249ff29401fa2e914c68f2555fdf
CHATSCD <shaundubuisson24@gmail.com>
2026-04-09 00:46:49 -0500

    Update index.ts

--- supabase/functions/process-checks/index.ts
+++ supabase/functions/process-checks/index.ts
@@ -10,8 +10,8 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 const supabase = createClient(
-  Deno.env.get('SUPABASE_URL')!,
-  Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!
+  Deno.env.get('https://aahfydouyyrvrcubwoxa.supabase.co')!,
+  Deno.env.get(eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhaGZ5ZG91eXlydnJjdWJ3b3hhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTcwNDYyMiwiZXhwIjoyMDkxMjgwNjIyfQ.B5NyDJmwc8N4fFg7C0IBrZHKYjllzCsp89gcxFp6IEU)!
 )
 
 Deno.serve(async (_req) => {
