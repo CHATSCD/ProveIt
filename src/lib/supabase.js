dd5d23aa9774d58f112f6eb7fc803c994c0a2b87
CHATSCD <shaundubuisson24@gmail.com>
2026-04-09 01:08:09 -0500

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://aahfydouyyrvrcubwoxa.supabase.co'
+const SUPABASE_URL = 'https://aahfydouyyrvrcubwoxa.supabase.co'F0xj1raQje0
 const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhaGZ5ZG91eXlydnJjdWJ3b3hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MDQ2MjIsImV4cCI6MjA5MTI4MDYyMn0.3vvBS83UNKVdDYBcoV4wr25d3StIdxb_F0xj1raQje0'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON)
